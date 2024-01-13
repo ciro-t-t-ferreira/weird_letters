17593982906f99d1e32e86926d8214b45c2ddfe0
Ciro Ferreira <ferreira.cirott@gmail.com>
2024-01-13 10:47:57 -0300

    remove debug window.alert

--- letters.js
+++ letters.js
@@ -1,39 +1,40 @@
-let checkbox = document.getElementById('transliterationBox');
+let checkboxTranslit = document.getElementById('transliterationBox');
 let checkboxLetter = document.getElementById('letterBox');
 
-let translits = document.querySelectorAll('.translits');
-let letters = document.querySelectorAll('.letters')
+checkboxTranslit.addEventListener('change', transliterationCheckbox);
+checkboxLetter.addEventListener('change', transliterationCheckbox);
 
-checkbox.addEventListener('change', transliterationCheckbox)
-checkboxLetter.addEventListener('change', transliterationCheckbox)
+let translits = document.querySelectorAll('.translits');
+let letters = document.querySelectorAll('.letters');
+
 
 function transliterationCheckbox(element){
     
     if(element == 'transliterationBox'){
         let visible;
         
-        if (checkbox.checked){
-            visible = true
-            translitsOnOff(visible, transliterationBox)              
+        if (checkboxTranslit.checked){            
+            visible = true;
+            translitsOnOff(visible, 'transliterationBox');              
         }
         
-        else{
-            visible = false
-            translitsOnOff(visible, transliterationBox)
+        else{            
+            visible = false;
+            translitsOnOff(visible, 'transliterationBox');
         }
     }
-
+    
     if(element == 'letterBox'){
         let visible;
         
-        if (checkbox.checked){
-            visible = true
-            translitsOnOff(visible, 'letterBox')              
+        if (checkboxLetter.checked){                    
+            visible = true;
+            translitsOnOff(visible, 'letterBox');              
         }
-
-        else{
-            visible = false
-            translitsOnOff(visible, 'letterBox')    
+    
+        else{                        
+            visible = false;
+            translitsOnOff(visible, 'letterBox');    
         }
     }
 }
@@ -43,21 +44,21 @@ function translitsOnOff(visible, elementName){
     if(elementName == 'transliterationBox'){
         for (let i=0; i < translits.length; i++){
             if (visible == true){
-                translits[i].style.display = 'initial'
+                translits[i].style.display = 'initial';
             }
             else if (visible == false){
-                translits[i].style.display = 'none'
+                translits[i].style.display = 'none';
             }        
         }
     }
 
     if(elementName == 'letterBox'){
         for (let i=0; i < letters.length; i++){
-            if (visible == true){
-                letters[i].style.display = 'initial'
+            if (visible == true){                
+                letters[i].style.display = 'inline';
             }
-            else if (visible == false){
-                letters[i].style.display = 'none'
+            else if (visible == false){                
+                letters[i].style.display = 'none';
             }        
         }
     }
